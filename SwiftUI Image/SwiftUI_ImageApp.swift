e92c056199c76d1c3603b90b3c693a32b37eb660
Jakir Hossain <jakirseu@gmail.com>
2023-09-23 23:32:51 +0600

    Initial Commit

---
+++ SwiftUI Image/SwiftUI_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ImageApp.swift
+//  SwiftUI Image
+//
+//  Created by Jakir Hossain on 23/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
